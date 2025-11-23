b026b342e5412f8a65e1064c11bc979e949fa3af
JovialBadger <68871905+JovialBadger@users.noreply.github.com>
2025-11-23 18:19:23 +0000

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -8,8 +8,8 @@
   // @match       *://*/*
   // @grant       none
   // @author      Jovial Badger
-  // @downloadURL {{ site.url }}{{site.baseurl}}{{page.url | relative_url }}
-  // @updateURL   {{ site.url }}{{site.baseurl}}{{page.url}}
+  // @downloadURL {{ site.url }}{{page.url | relative_url }}
+  // @updateURL   {{ site.url }}{{page.url | relative_url }}
   // @homepageURL {{ site.url }}{{site.baseurl}}
   // @icon        {{ site.url }}{{ "/assets/logo/letters_logo.svg" | relative_url }}
   // @run-at      document-end
